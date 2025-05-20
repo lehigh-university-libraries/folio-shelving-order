fef1f6ae1d5f0f3cd5af9ee59aeb5d6858bc6ef2
Joe Corall <jjc223@lehigh.edu>
2025-05-20 11:48:01 -0400

    move GHA YML into test

--- ci/test.sh
+++ ci/test.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+docker build -t fso:latest .
+docker run --rm -d --name fso -p 8080:8080 fso:latest
+sleep 10
+
 declare -A tests=(
   ["792.0944+T374+v.1"]="3792.0944 T374 V 11"
   ["792.0944+T374+v.2"]="3792.0944 T374 V 12"
   ["792.0944+T3741"]="3792.0944 T3741"
-  ["792.0944+T3741"]="3792.0944 T3742"
 )
 
 BASE_URL="http://localhost:8080/folio-shelving-order/shelf-key?callNumber="
@@ -13,12 +16,15 @@ PASS=true
 for callNumber in "${!tests[@]}"; do
   expected="${tests[$callNumber]}"
   response=$(curl -fs "${BASE_URL}${callNumber}")
-
+  status=OK
   if [[ "$response" != "$expected" ]]; then
-    echo "FAIL: '$callNumber' => got '$response', expected '$expected'"
+    status=FAIL
     PASS=false
   fi
+  echo "$callNumber => $response $status"
 done
+
+# docker stop fso > dev/null  2>&1
 
 if ! $PASS; then
   exit 1
